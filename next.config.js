61f216d12461ab1ae6c8d62015bae2a62767fec5
Saksham8006 <109299688+Saksham8006@users.noreply.github.com>
2024-01-02 13:17:25 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// next.config.js
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Configure client-specific optimizations
+      config.optimization.splitChunks.cacheGroups = {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      };
+    }
 
-module.exports = nextConfig
+    return config;
+  },
+};
